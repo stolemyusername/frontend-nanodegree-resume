79d1750c180d72f440261453ba0f9333864b3b76
Guy Thomas <guythomas721@gmail.com>
2016-06-05 21:05:35 +1000

    Add learnings folder

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,8 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var name = "Guy";
+var role = "Web Developer"
+
+var formattedName = HTMLheaderName.replace("%data%",name);
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+$("#header").prepend(formattedName)
+$("#header").prepend(formattedRole)
