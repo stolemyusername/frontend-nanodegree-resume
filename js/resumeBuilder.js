5129cd5072f24fdbe42fbfc752201255b700ca2e
Guy Thomas <guythomas721@gmail.com>
2016-06-06 11:12:57 +1000

    Adding learnings from problem set 1

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,8 +1,68 @@
-var name = "Guy";
-var role = "Web Developer"
+var bio = {
+	"name": "Guy",
+	"role": "Web Developer",
+	"contacts": {
+		"mobile": "61-430-706-927",
+		"email": "guythomas721@gmail.com",
+		"github": "stolemyusername",
+		"location": "Melbourne, Australia"
+	},
+	"welcome-message": "Welcome to my site",
+	"skills": ["Drone Pilot", "Rubik's Cube"],
+	"biopic": ""
+}
 
-var formattedName = HTMLheaderName.replace("%data%",name);
-var formattedRole = HTMLheaderRole.replace("%data%", role);
 
-$("#header").prepend(formattedName)
-$("#header").prepend(formattedRole)
+
+var work = {
+	"jobs": [{
+		"employer": "IBM",
+		"position": "Graduate Consultant",
+		"location": "Melbourne",
+		"dates": "July 2014 -> June 2016",
+		"description": "Project manager on many small projects (<5 people)."
+	}]
+};
+
+
+
+var education = {
+	"schools": [{
+		"name": "Monash University",
+		"location": "Melbourne",
+		"degree": "Bachelor of Commerce",
+		"majors": ["Finance", "Economics"],
+		"dates": "Dec 2013",
+		"url": "www.study.monash"
+	}, {
+		"name": "Vermont Secondary College",
+		"location": "Melbourne",
+		"degree": "VCE",
+		"majors": ["Physics", "Economics"],
+		"dates": "Dec 2010",
+		"url": "www.vermontsc.vic.edu.au/"
+	}],
+	"onlineCourses": [{
+		"name": "Intro To Javascript",
+		"topic": "Programming",
+		"dates": "June 2016",
+		"url": "https://www.udacity.com/course/javascript-basics--ud804"
+	}, {
+		"name": "Intro To Computer Science",
+		"topic": "Programming",
+		"dates": "May 2016",
+		"url": "https://www.edx.org/course/introduction-computer-science-mitx-6-00-1x-7"
+	}]
+};
+
+var projects = {
+	"projects": [{
+		"title": "ANZ Integrated Account",
+		"dates": "March, 2016 -> June, 2016",
+		"description": "PMO"
+	}, {
+		"title": "Unica",
+		"dates": "May, 2015 -> March, 2016",
+		"description": "Data Analyst"
+	}]
+}
